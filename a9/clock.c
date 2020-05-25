069117aeee8f8f5f5af95beac2085d0d4208d031
khushmeet29 <56785817+khushmeet29@users.noreply.github.com>
2020-05-25 18:35:01 +0530

    Update clock.c

--- a9/clock.c
+++ a9/clock.c
@@ -55,7 +55,7 @@ int main() {
 	c = pthread_create(&t3, NULL, hour, NULL);
 	d = pthread_create(&t4, NULL, print, NULL);
 	
-	
+	pthread_join(t1, NULL);
 	pthread_join(t2, NULL);
 	pthread_join(t3, NULL);
 	pthread_join(t4, NULL);
